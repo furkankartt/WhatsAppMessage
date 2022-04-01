2d9a3da8154abeed4e6f485eb2da82ba3c3a4b87
Furkan KART <planlama11@berteks.com>
2022-04-01 16:25:22 +0300

    Version 1

--- WhatsappMessage/Form1.Designer.cs
+++ WhatsappMessage/Form1.Designer.cs
@@ -29,12 +29,100 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.textBoxNo = new System.Windows.Forms.TextBox();
+            this.textBoxIcerik = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonGonder = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBoxNo
+            // 
+            this.textBoxNo.Location = new System.Drawing.Point(138, 11);
+            this.textBoxNo.Name = "textBoxNo";
+            this.textBoxNo.Size = new System.Drawing.Size(274, 22);
+            this.textBoxNo.TabIndex = 0;
+            // 
+            // textBoxIcerik
+            // 
+            this.textBoxIcerik.Location = new System.Drawing.Point(138, 44);
+            this.textBoxIcerik.Multiline = true;
+            this.textBoxIcerik.Name = "textBoxIcerik";
+            this.textBoxIcerik.Size = new System.Drawing.Size(274, 106);
+            this.textBoxIcerik.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Gönderilecek No:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Messaj İçeriği:";
+            // 
+            // buttonGonder
+            // 
+            this.buttonGonder.Location = new System.Drawing.Point(264, 159);
+            this.buttonGonder.Name = "buttonGonder";
+            this.buttonGonder.Size = new System.Drawing.Size(148, 50);
+            this.buttonGonder.TabIndex = 4;
+            this.buttonGonder.Text = "Mesaj Gönder";
+            this.buttonGonder.UseVisualStyleBackColor = true;
+            this.buttonGonder.Click += new System.EventHandler(this.buttonGonder_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(112, 100);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(20, 50);
+            this.webBrowser1.TabIndex = 5;
+            this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(424, 223);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.buttonGonder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxIcerik);
+            this.Controls.Add(this.textBoxNo);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxNo;
+        private System.Windows.Forms.TextBox textBoxIcerik;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonGonder;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- WhatsappMessage/Form1.cs
+++ WhatsappMessage/Form1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace WhatsappMessage
 {
@@ -16,5 +12,112 @@ namespace WhatsappMessage
         {
             InitializeComponent();
         }
+        
+        public void CMDCalistir(string fileName, string workingDirectory, string arguments, ProcessWindowStyle style)
+        {
+            var p2 = new Process
+            {
+                StartInfo =
+                 {
+                     FileName = fileName,
+                     WorkingDirectory = workingDirectory,
+                     Arguments = arguments,
+                     WindowStyle = style
+                 }
+            }.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SendKeys.Send("%{TAB}");
+            SendKeys.Send("{ENTER}");
+        }
+
+        private void sendWhatsApp(string number, string message)
+        {
+            try
+            {
+                if(number == "")
+                {
+                    MessageBox.Show("Numara giriniz..");
+                    textBoxNo.Focus();
+                }
+                else if(number.Length <= 10)
+                {
+                    List<string> no = new List<string>();
+                    number = "+90" + number;
+                    number = number.Replace(" ", "");
+                    no.Add(number);
+                    no.Add("+905384452384");
+                    no.Add("+905345448169");
+                    no.Add("+905384452384");
+                    no.Add("+905345448169");
+                    no.Add("+905384452384");
+                    no.Add("+905345448169");
+                    for (int i = 0; i < no.Count; i++)
+                    {
+                        webBrowser1.Url = new Uri("https://api.whatsapp.com/send?phone=" + no[i] + "&text=" + message);
+                        timer1.Start();
+                        MessageBox.Show(no[i] + " numaralı kişiye mesajınız gönderildi.", "BİLGİ");
+                        SendKeys.Send("%{TAB}");
+                        SendKeys.Send("{ENTER}");
+                        timer1.Stop();
+                        #region SendKey
+
+                        //foreach (var process in Process.GetProcessesByName("chrome"))
+                        //{
+                        //    process.Kill();
+                        //}
+                        //SendKeys.Send("{ENTER}");
+                        //System.Threading.Thread.Sleep(5000);
+                        //SendKeys.Send("%{F4}");
+                        //System.Threading.Thread.Sleep(2000);
+                        //SendKeys.Send("^{W}");
+                        //System.Threading.Thread.Sleep(2000);
+                        #endregion
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: ", ex.ToString());
+            }
+        }
+
+        private void buttonGonder_Click(object sender, EventArgs e)
+        {
+            sendWhatsApp(textBoxNo.Text, textBoxIcerik.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            #region Değişkenler
+            string userDesktop = System.Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string userLocal = System.Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            bool Kontrol = Directory.Exists(userLocal + "\\WhatsApp");
+            string KopyalanacakDosyaYolu = @"\\192.168.100.17\Ses_Dosyalari";
+            string KaydedilecekDosyaYolu = @userDesktop + "\\WhatsAppSetup.exe";
+            string[] allFiles = Directory.GetFiles(KopyalanacakDosyaYolu, "WhatsAppSetup.exe", SearchOption.AllDirectories);
+            #endregion
+
+            if (Kontrol)
+            {
+                Process.Start(userLocal + "/WhatsApp/WhatsApp.exe");
+            }
+            else
+            {
+                foreach (var file in allFiles)
+                {
+                    FileInfo fi = new FileInfo(file);
+                    fi.CopyTo(KaydedilecekDosyaYolu, true);
+                }
+
+                CMDCalistir("WhatsAppSetup.exe", userDesktop, null, ProcessWindowStyle.Normal);
+                MessageBox.Show("Whatsapp yüklü değil kurulumu tamamlayınız.");
+            }
+            this.TopMost = true;
+            webBrowser1.Hide();
+        }
+
     }
 }
